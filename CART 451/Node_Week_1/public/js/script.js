634a6707c44a82d1a1c64123a3d3d6fc877c7a6c
Anon_01 <57970996+ElCheGuapo@users.noreply.github.com>
2022-09-13 17:19:18 -0400

    Update script.js

--- CART 451/Node_Week_1/public/js/script.js
+++ CART 451/Node_Week_1/public/js/script.js
@@ -18,21 +18,26 @@ function setup() {
 
     client.connect();
 
+    let userInDB = false;
     client.on('message', (channel, tags, message, self) => {
 	    console.log(`${tags['display-name']}: ${message}`);
         for(user of users) {
             if('display-name' == user) {
-                console.log('user in database');
+                userInDB = true;
             }
+        }
+        if(!userInDB) {
+            console.log('creating new user');
+        } else if (userInDB) {
+            console.log('user in database');
+            userInDB = false;
         }
     });
 }
 
-
 function createUser(_user, _chatcount, _profilepic) {
     let tempUser = new User(_user, _chatcount, _profilepic);
 }
-
 
 function draw() {
     background(0);
